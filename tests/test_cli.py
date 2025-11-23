ed9401e755beb95be7d935a4d59a407936e6d0c1
Matthew Brett <matthew.brett@gmail.com>
2025-11-23 08:54:35 +0000

    Refactor cli testing.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,16 +13,22 @@ HERE = Path(__file__).parent
 def test_main(tmp_path):
     config_file = str(HERE / 'example__config.yml')
     support_files = [str(HERE / '..' / 'src'), config_file]
-    out1 = tmp_path / 'out1'
-    start_args = [str(out1)] + support_files + ['--config', config_file]
-    explicit_args = start_args + ['--jl-tmp', str(tmp_path / 'jl')]
-    for args in start_args, explicit_args:
-        with patch.object(sys, 'argv', [''] + args):
-            main_cli()
-        assert out1.is_dir()
-        assert (out1 / 'notebooks').is_dir()
-        f_pth = (out1 / 'files')
-        assert (f_pth / 'src').is_dir()
-        assert (f_pth / 'example__config.yml').is_file()
-        assert (f_pth / 'eg.ipynb').is_file()
-        assert (f_pth / 'eg2.ipynb').is_file()
+    start_args = support_files + ['--config', config_file]
+    # Defaults to temporary JL scratch directory.
+    check_jl_cmd(tmp_path / 'out1', start_args)
+    # Explicit JL scratch directory.
+    jl_out = tmp_path / 'jl'
+    check_jl_cmd(tmp_path / 'out2', start_args + ['--jl-tmp', str(jl_out)])
+    assert (jl_out / 'eg.ipynb').is_file()
+
+
+def check_jl_cmd(out_path, args):
+    with patch.object(sys, 'argv', ['', str(out_path)] + args):
+        main_cli()
+    assert out_path.is_dir()
+    assert (out_path / 'notebooks').is_dir()
+    f_pth = (out_path / 'files')
+    assert (f_pth / 'src').is_dir()
+    assert (f_pth / 'example__config.yml').is_file()
+    assert (f_pth / 'eg.ipynb').is_file()
+    assert (f_pth / 'eg2.ipynb').is_file()
